2fb9225e32d22982f047b9f89e8aff0b94621916
evgeniy <evgeny.vinokurov@yoso.ru>
2025-01-13 00:04:37 +0300

    updated

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    server: {
+        port: 8000
+    },
+    plugins: [vue()]
 })
